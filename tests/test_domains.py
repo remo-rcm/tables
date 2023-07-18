8cac3b593bf018109d8e44691a143f92d95d9b7e
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2023-07-18 14:36:52 +0200

    run pre-commit

--- tests/test_domains.py
+++ tests/test_domains.py
@@ -1,16 +1,17 @@
-
-import pytest
-import pandas as pd
 import cordex as cx
+import pandas as pd
+import pytest
 
 url = "./domains/domains.csv"
 
+
 @pytest.fixture
 def domain_table():
-    return pd.read_csv(url).set_index('domain_id')
+    return pd.read_csv(url).set_index("domain_id")
+
 
 def test_table(domain_table):
     df = domain_table
     for domain_id in df.index:
         print(domain_id)
-        ds = cx.cordex_domain(domain_id, tables=df)
+        assert cx.cordex_domain(domain_id, tables=df)
