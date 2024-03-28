10f6ae7854e82feb4003a8e03739f6c824e2e67b
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-03-28 11:49:27 +0100

    Update test_domains.py

--- tests/test_domains.py
+++ tests/test_domains.py
@@ -20,7 +20,7 @@ def test_table(domain_table):
 def test_magic_numbers(domain_table):
     # test if nlon and nlat are magic number
     magic_numbers = pr.magic_numbers()
-    print("magic_numbers: magic_numbers")
+    print(f"magic_numbers: {magic_numbers}")
     df = domain_table
     for domain_id, row in df.iterrows():
         assert row.nlon in magic_numbers
